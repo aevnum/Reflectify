1db68d0c110d878542ffa9af797ccc685b14f883
jaybaradd <jaydharmishtha@gmail.com>
2024-04-05 21:19:02 +0530

    created notesactivity and createnote

--- app/src/main/java/com/example/reflectify/notesactivity.java
+++ app/src/main/java/com/example/reflectify/notesactivity.java
@@ -1,24 +1,65 @@
 package com.example.reflectify;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class notesactivity extends AppCompatActivity {
 
+    FloatingActionButton mcreatenotesfab;
+
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_notesactivity);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+        mcreatenotesfab = findViewById(R.id.createnotefab);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        getSupportActionBar().setTitle("All Notes");
+
+        mcreatenotesfab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(notesactivity.this, createnote.class));
+            }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId())
+        {
+            case R.id.Logout:
+                firebaseAuth.signOut();
+                finish();
+                startActivity(new Intent(notesactivity.this, MainActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
