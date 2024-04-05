dc28fdded1672bf6bbb5b3edb88975ab2860db9e
jaybaradd <jaydharmishtha@gmail.com>
2024-04-05 19:50:12 +0530

    updated signup, main, and forgotpassword java

--- app/src/main/java/com/example/reflectify/forgotpassword.java
+++ app/src/main/java/com/example/reflectify/forgotpassword.java
@@ -1,14 +1,53 @@
 package com.example.reflectify;
 
+import static com.example.reflectify.R.id.passwordrecoverbutton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class forgotpassword extends AppCompatActivity {
 
+    private EditText mforgotpassword;
+    private Button mpasswordrecoverbutton;
+    private TextView mgobacktologin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgotpassword);
+
+        getSupportActionBar().hide();
+
+        mforgotpassword = findViewById(R.id.forgotpassword);
+        mpasswordrecoverbutton = findViewById(R.id.passwordrecoverbutton);
+
+        mgobacktologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(forgotpassword.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mpasswordrecoverbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail = mforgotpassword.getText().toString().trim();
+                if(mail.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(), "Enter your mail id", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    //send email
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/reflectify/signup.java
+++ app/src/main/java/com/example/reflectify/signup.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -36,7 +37,36 @@ public class signup extends AppCompatActivity implements View.OnClickListener {
         msignup = findViewById(R.id.signup);
         mgotologin = findViewById(R.id.gotologin);
 
-        mgotologin.setOnClickListener(this);
+        mgotologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(signup.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        msignup.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                String mail = msignupemail.getText().toString().trim();
+                String password = msignuppassword.getText().toString().trim();
+
+                if(mail.isEmpty() || password.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(), "All fields are required", Toast.LENGTH_SHORT).show();
+                }
+                else if(password.length()<7)
+                {
+                    Toast.makeText(getApplicationContext(), "Password shouold be more than 7 characters", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    //register to firebase
+                }
+
+            }
+        });
     }
 
     @Override
