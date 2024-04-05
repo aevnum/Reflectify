833d0bde96e3f4ff7cbe13eb1c7cfb9e392c1f8b
Kshitij Nevrekar <71255066+aevnum@users.noreply.github.com>
2024-04-06 00:36:15 +0530

    small issue in model

--- app/src/main/java/com/example/reflectify/firebasemodel.java
+++ app/src/main/java/com/example/reflectify/firebasemodel.java
@@ -24,7 +24,7 @@ public class firebasemodel {
     {
         return content;
     }
-    public void setContent()
+    public void setContent(String content)
     {
         this.content = content;
     }
